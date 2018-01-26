d57cf24228881aeef776fd6df3c7ea0fafb1d9d4
Leon Guo <chineseroyalstar@gmail.com>
2018-01-26 15:19:56 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/ZXMarsKit/ZXMarsKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZXMarsKit : NSObject
+@end
+@implementation PodsDummy_ZXMarsKit
+@end
---
+++ ZXMarsKit/Classes/UILabel+AttachementText.h
@@ -0,0 +1,17 @@
+//
+//  UILabel+AttachementText.h
+//  SJDFBackstage
+//
+//  Created by armada on 2018/1/16.
+//  Copyright © 2018年 Leon Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (AttachementText)
+
+@property(nonatomic, strong) NSString *prefixText;
+
+@property(nonatomic, strong) NSString *suffixText;
+
+@end
